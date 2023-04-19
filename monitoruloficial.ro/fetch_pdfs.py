1a75a09f0a016f64414239f77946cc40b437112b
pax <pax@mioritics.ro>
2023-04-19 14:31:53 +0300

    fetching checks if file exists before scraping

--- monitoruloficial.ro/fetch_pdfs.py
+++ monitoruloficial.ro/fetch_pdfs.py
@@ -8,47 +8,55 @@ from common import base_headers
 """ 
 read each json
 download pdfs
+check if exists, mode overwrite, mode update
  """
 
-db_filename = '../data/mo/mo.db'
-table_name = 'dates_lists'
-output_folder = '../data/mo/pdfs/'
-url_base = 'https://monitoruloficial.ro'
-verbose = False
+db_filename     =   '../../data/mo/mo.db'
+table_name      =   'dates_lists'
+output_folder   =   '../../data/mo/pdfs/'
+verbose         =   False
+overwrite       =   False   # if false check if file exists, don't fetch again
+pause_at        =   30      # days
+pause           =   10      # seconds
+url_base        =   'https://monitoruloficial.ro'
 
-# Connect to the SQLite database
+#  - - - - - - - - - - - - - - - - - - - - -  
+
 conn = sqlite3.connect(db_filename)
 c = conn.cursor()
-
-# Fetch all rows from the "dates_lists" table
 c.execute('SELECT * FROM ' + table_name)
 rows = c.fetchall()
- 
 
 pbar = tqdm(len(rows))
 nrows = len(rows)
 tqdm.write('zz ' + str(nrows))
 # Iterate over the rows and convert the JSON string to a dictionary
+all_files = 0
 for row in tqdm(rows):
     date, json_str = row
     json_dict = json.loads(json_str)
-    # print(f"Date: {date}, JSON dictionary: {json_dict}")
-    # breakpoint()
     ii = 0
-     
+    files_found = 0
     for sectiuni, parti in json_dict.items():
         for nr, url in parti.items():
             jj = 0
             # download pdf
-            # filename = url[1:]
             filename = os.path.splitext(url[1:])[0]
+            urlparts = filename.split('--')
 
-            urlparts = url.split('--')
-            # print(date + ' [' + urlparts[1] + '] ' + sectiuni + ' : ' + str(nr) + ' ' + url)
-            # tqdm.write(date + ' [' + urlparts[1] + '] ' + str(nr) + '  - - -  ' + url)
-            # TODO: save to log
+            year=urlparts[-1]
+            if len(year) != 4:
+                tqdm.write('err: ' + str(year))
+
             if verbose:
                 tqdm.write('>> ' + url_base + url)
+
+            if overwrite is False and os.path.isfile(output_folder + str(year) + '/' + filename + '.pdf'):
+                files_found += 1
+                if verbose:
+                    tqdm.write('skipping ' + filename + '.pdf');
+                continue #if overwrite = False and file exists, continue
+               
             try:
                 response = requests.get(url_base + url, headers=base_headers('headers2'))
             except Exception as e:
@@ -57,14 +65,19 @@ for row in tqdm(rows):
             # save pdf
             try:
                 # with open(output_folder + date + '.pdf', 'wb') as f:
-                with open(output_folder + filename + '.pdf', 'wb') as f:
+                with open(output_folder + year + '/' +  filename + '.pdf', 'wb') as f:
                     f.write(response.content)
+                    all_files += 1
+                    time.sleep(random.random()*1.25)
                 if verbose:
                     tqdm.write(' > > ' + filename + 'pdf')
             except Exception as e:
                 logging.error(f'Error saving PDF URL {url_base + url}: {e}')
-            time.sleep(random.random()*1.75)
+            
             jj+=1
+            if round(all_files/pause_at) == all_files/pause_at:
+                time.sleep(pause)
+                os.system('say -v ioana "piiua " -r 250')
             # tqdm.write('   - >  ' + str(ii) + '/' + str(nrows) + ' ' + str(ii+jj) + ' ' + str(jj))
     ii+=1
     
@@ -72,3 +85,5 @@ for row in tqdm(rows):
 # Close the database connection
 conn.close()
 tqdm.write('done ' + str(len(rows)) + ' days ' + str(ii) + 'secțiuni' + str(jj) + 'saved pdfs')
+
+os.system('say -v ioana "în sfârșit, am gătat ' +  str(all_files) + ' fișiere " -r 250')
