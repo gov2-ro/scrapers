7b83e5938593dab0bcc48521f3f8105f756e9bfd
pax <pax@mioritics.ro>
2023-10-02 12:26:38 +0300

    attempt to fix the pbar

--- wp-rake/batch-rake.py
+++ wp-rake/batch-rake.py
@@ -6,10 +6,10 @@ import re
 from tqdm import tqdm
 
 # Define the SQLite database file
-dbFile = '../../data/wp/wp-articles6.db'
+dbFile = '../../data/wp/wp-articles8.db'
 
 # Read the list of sites from the CSV file
-sitelist = '../../data/wp/sites-wp.csv'
+sitelist = '../../data/wp/count.csv'
 url_column = 2
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36"
@@ -135,102 +135,105 @@ error_count = 0
 # Initialize live counters
 total_articles = 0
 fetch_errors = 0
+pbar = tqdm(total=total_urls, desc="Sites", dynamic_ncols=True)
 
 # Create a tqdm progress bar
-with tqdm(total=total_urls, desc="Sites", dynamic_ncols=True) as pbar:
-    # Loop through the list of sites
-    with open(sitelist, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)  # Skip the header row
-        for row in reader:
-            site_url = row[url_column]
-            parsed_domain = re.sub(r'^(https?://)?(www\.)?', '', site_url)
-            tag_slugs_cache = {}  # Clear the tag slugs cache for each site
-            category_slugs_cache = {}  # Clear the category slugs cache for each site
+# with tqdm(total=total_urls, desc="Sites", dynamic_ncols=True) as pbar:
+with open(sitelist, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # Skip the header row
 
-            # Construct the API URL
-            api_url = f"{site_url}/wp-json/wp/v2/posts"
+    for row in reader:
+        site_url = row[url_column]
+        parsed_domain = re.sub(r'^(https?://)?(www\.)?', '', site_url)
+        tag_slugs_cache = {}  # Clear the tag slugs cache for each site
+        category_slugs_cache = {}  # Clear the category slugs cache for each site
 
-            page_number = 1
+        # Construct the API URL
+        api_url = f"{site_url}/wp-json/wp/v2/posts"
 
-            while True:
-                # Make a request for the current page
-                params = {"page": page_number}
-                response = requests.get(api_url, headers=headers, params=params)
+        page_number = 1
 
-                try:
-                    response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
-                    data = response.json()
+        while True:
+            # Make a request for the current page
+            params = {"page": page_number}
+            response = requests.get(api_url, headers=headers, params=params)
 
-                    if not data:
-                        # No more data available, exit the loop
-                        break
+            try:
+                response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+                data = response.json()
 
-                    # Save data to the SQLite database for each article
-                    for article_data in data:
-                        article_count += 1
-                        post_id = article_data["id"]
-                        # Fetch tags and categories
-                        tags = fetch_tags(post_id, site_url)
-                        categories = fetch_categories(post_id, site_url)
-
-                        # Update the taxonomies table for tags
-                        for tag_slug in tags:
-                            update_taxonomies(parsed_domain, "tag", tag_slug, post_id)
-
-                        # Update the taxonomies table for categories
-                        for category_slug in categories:
-                            update_taxonomies(parsed_domain, "category", category_slug, post_id)
-
-                        cursor.execute('''
-                            INSERT INTO articles (domain, date, date_gmt, guid, modified, modified_gmt, slug, status, type, link, title, content, excerpt, author, featured_media, comment_status, ping_status, sticky, template, format, categories, tags)
-                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                        ''', (
-                            parsed_domain,
-                            article_data["date"],
-                            article_data["date_gmt"],
-                            article_data["guid"]["rendered"],
-                            article_data["modified"],
-                            article_data["modified_gmt"],
-                            article_data["slug"],
-                            article_data["status"],
-                            article_data["type"],
-                            article_data["link"],
-                            article_data["title"]["rendered"],
-                            article_data["content"]["rendered"],
-                            article_data["excerpt"]["rendered"],
-                            article_data["author"],
-                            article_data["featured_media"],
-                            article_data["comment_status"],
-                            article_data["ping_status"],
-                            article_data["sticky"],
-                            article_data["template"],
-                            article_data["format"],
-                            json.dumps(categories),
-                            json.dumps(tags)
-                        ))
-                        total_articles += 1
-
-                    conn.commit()
-
-                    # Update the live counters in the progress bar description
-                    pbar.set_description(f"Sites: {site_count}, Articles: {total_articles}, Fetch Errors: {fetch_errors}")
-
-                    # Move to the next page
-                    page_number += 1
-
-                except requests.exceptions.RequestException as e:
-                    print(f"Request failed with exception: {e}")
-                    error_count += 1
-                    fetch_errors += 1
-                    break
-                except json.JSONDecodeError as e:
-                    print(f"Failed to decode JSON with exception: {e}")
-                    error_count += 1
-                    fetch_errors += 1
+                if not data:
+                    # No more data available, exit the loop
                     break
 
-            site_count += 1
+                # Save data to the SQLite database for each article
+                for article_data in data:
+                    article_count += 1
+                    post_id = article_data["id"]
+                    # Fetch tags and categories
+                    tags = fetch_tags(post_id, site_url)
+                    categories = fetch_categories(post_id, site_url)
+
+                    # Update the taxonomies table for tags
+                    for tag_slug in tags:
+                        update_taxonomies(parsed_domain, "tag", tag_slug, post_id)
+
+                    # Update the taxonomies table for categories
+                    for category_slug in categories:
+                        update_taxonomies(parsed_domain, "category", category_slug, post_id)
+
+                    cursor.execute('''
+                        INSERT INTO articles (domain, date, date_gmt, guid, modified, modified_gmt, slug, status, type, link, title, content, excerpt, author, featured_media, comment_status, ping_status, sticky, template, format, categories, tags)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    ''', (
+                        parsed_domain,
+                        article_data["date"],
+                        article_data["date_gmt"],
+                        article_data["guid"]["rendered"],
+                        article_data["modified"],
+                        article_data["modified_gmt"],
+                        article_data["slug"],
+                        article_data["status"],
+                        article_data["type"],
+                        article_data["link"],
+                        article_data["title"]["rendered"],
+                        article_data["content"]["rendered"],
+                        article_data["excerpt"]["rendered"],
+                        article_data["author"],
+                        article_data["featured_media"],
+                        article_data["comment_status"],
+                        article_data["ping_status"],
+                        article_data["sticky"],
+                        article_data["template"],
+                        article_data["format"],
+                        json.dumps(categories),
+                        json.dumps(tags)
+                    ))
+                    total_articles += 1
+                
+                
+
+                conn.commit()
+
+                # Update the live counters in the progress bar description
+                pbar.set_description(f"Sites: {site_count}, Articles: {total_articles}, Fetch Errors: {fetch_errors}")
+
+                # Move to the next page
+                page_number += 1
+
+            except requests.exceptions.RequestException as e:
+                print(f"Request failed with exception: {e}")
+                error_count += 1
+                fetch_errors += 1
+                break
+            except json.JSONDecodeError as e:
+                print(f"Failed to decode JSON with exception: {e}")
+                error_count += 1
+                fetch_errors += 1
+                break
+
+        site_count += 1
 
 # Close the SQLite connection
 conn.close()
